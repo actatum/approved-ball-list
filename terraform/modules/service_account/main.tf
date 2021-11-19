34de14bba1cb24b2c4aa069d494caf98c0793065
actatum <actatum@umich.edu>
2021-11-19 17:39:40 -0500

    adding more iam perms

--- terraform/modules/service_account/main.tf
+++ terraform/modules/service_account/main.tf
@@ -30,6 +30,24 @@ resource "google_project_iam_binding" "circleci-pubsub" {
   ]
 }
 
+resource "google_project_iam_binding" "circleci-cloud-scheduler" {
+  project = var.project
+  role    = "roles/cloudscheduler.admin"
+
+  members = [
+    "serviceAccount:${google_service_account.circleci.email}",
+  ]
+}
+
+resource "google_project_iam_binding" "circleci-iam" {
+  project = var.project
+  role    = "roles/iam.securityAdmin"
+
+  members = [
+    "serviceAccount:${google_service_account.circleci.email}",
+  ]
+}
+
 resource "google_service_account_key" "circleci_key" {
   service_account_id = google_service_account.circleci.name
   public_key_type    = "TYPE_X509_PEM_FILE"
