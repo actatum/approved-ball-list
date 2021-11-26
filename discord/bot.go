37d29f082b1d49808a9f8e082e71044c1cd45498
actatum <actatum@umich.edu>
2021-11-26 17:39:32 -0500

    fixing discord formatting for individual messages, also updating batch inserts (#2)

--- discord/bot.go
+++ discord/bot.go
@@ -2,15 +2,14 @@ package discord
 
 import (
 	"fmt"
-	"html"
+	"os"
 
 	"github.com/actatum/approved-ball-list/models"
 	"github.com/bwmarrin/discordgo"
-	"github.com/gomarkdown/markdown"
 )
 
-var token = "OTExMDgwODE0ODgyNzI1OTM5.YZcMIQ.kbp-gqqkGdmyNuGbS4vSaKuz6SA"
-var channelID = "77164508690264064"
+var token = os.Getenv("DISCORD_TOKEN")
+var channelID = os.Getenv("CHANNEL_ID")
 
 // SendNewBalls sends the most recently added balls to discord
 func SendNewBalls(balls []models.Ball) error {
@@ -19,54 +18,26 @@ func SendNewBalls(balls []models.Ball) error {
 		ballMap[b.Brand] = append(ballMap[b.Brand], b)
 	}
 
-	fmt.Println(ballMap)
-
-	content := ""
-	for k, v := range ballMap {
-		content += fmt.Sprintf("**%s**", k)
-		// ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
-		for idx, b := range v {
-			content += "\n"
-			content += fmt.Sprintf("%d. %s\n", idx+1, b.Name)
-			// content += b.ImageURL
-			// content += "\n```hi```"
-			content += fmt.Sprintf("![%s](%s)", b.Brand+" "+b.Name, b.ImageURL)
-			// fmt.Println(b)
-		}
-	}
-	content = html.EscapeString(content)
-
-	fmt.Println(content)
-
-	output := markdown.ToHTML([]byte(content), nil, nil)
-	fmt.Println(string(output))
-
 	// Create a new Discord session using the provided bot token.
-	dg, err := discordgo.New("Bot " + token)
+	dg, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 	if err != nil {
 		return fmt.Errorf("discordgo.New: %w", err)
 	}
 
-	// msg, err := dg.ChannelMessageSendEmbed(ChannelID, &discordgo.MessageEmbed{
-	// 	Title: "New Approved Balls",
-	// 	Type:  discordgo.EmbedTypeImage,
-	// 	Fields: []*discordgo.MessageEmbedField{
-	// 		{
-	// 			Name:  "hi",
-	// 			Value: "hi",
-	// 		},
-	// 	},
-	// })
-	// if err != nil {
-	// 	return fmt.Errorf("dg.ChannelMessageSendEmbed: %w", err)
-	// }
-
-	msg, err := dg.ChannelMessageSend(channelID, content)
-	if err != nil {
-		return fmt.Errorf("dg.ChannelMessageSend: %w", err)
+	for _, v := range ballMap {
+		for _, b := range v {
+			_, err := dg.ChannelMessageSendEmbed(channelID, &discordgo.MessageEmbed{
+				Title: fmt.Sprintf("%s %s", b.Brand, b.Name),
+				Type:  discordgo.EmbedTypeImage,
+				Image: &discordgo.MessageEmbedImage{
+					URL: b.ImageURL,
+				},
+			})
+			if err != nil {
+				return fmt.Errorf("dg.ChannelMessageSendEmbed: %w", err)
+			}
+		}
 	}
-
-	fmt.Println(msg.ID)
 
 	return nil
 }
