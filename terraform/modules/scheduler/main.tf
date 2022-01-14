84c7452fc224bbb27f8e8daca82af7987d8b8303
actatum <actatum@umich.edu>
2022-01-13 21:51:53 -0500

    updating cloud scheduler job to run 3 times a day at 12pm, 5pm and 10pm est

--- terraform/modules/scheduler/main.tf
+++ terraform/modules/scheduler/main.tf
@@ -7,7 +7,7 @@ resource "google_cloud_scheduler_job" "job" {
   project     = var.project
   name        = "approvedBallListJob"
   description = "Run once weekly to check for new approved balls"
-  schedule    = "0 12 * * *"
+  schedule    = "0 12,17,22 * * *"
   time_zone   = "EST"
   region      = var.region
 
