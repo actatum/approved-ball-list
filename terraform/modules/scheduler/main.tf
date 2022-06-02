d0338a786dfebbc45bd156671642be4f4aa455d1
actatum <actatum@umich.edu>
2022-06-01 23:50:34 -0400

    fix description of cloud scheduler job

--- terraform/modules/scheduler/main.tf
+++ terraform/modules/scheduler/main.tf
@@ -1,7 +1,7 @@
 resource "google_cloud_scheduler_job" "job" {
   project     = var.project
   name        = "approvedBallListJob"
-  description = "Run once weekly to check for new approved balls"
+  description = "Run once hourly to check for new approved balls"
   schedule    = "0 * * * *"
   time_zone   = "EST"
   region      = var.region
